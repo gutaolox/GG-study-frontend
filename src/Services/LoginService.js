0ef95dacf4e6209bc1c33fb4b4f545f63d79e107
GiovannePrestes <41448106+GiovannePrestes@users.noreply.github.com>
2021-04-05 22:56:04 -0300

    eslint

--- src/Services/LoginService.js
+++ src/Services/LoginService.js
@@ -1,24 +1,25 @@
-import { customAxios } from "../Utils/customAxios";
+import { customAxios } from '../Utils/customAxios';
 
 export const login = (username, password, location, history) => {
   const newAxios = customAxios(false);
   return newAxios
-    .post("/auth/login", {
+    .post('/auth/login', {
       username,
       password,
     })
+
     .then((result) => {
       const { data } = result;
-      localStorage.setItem("token", data.access_token);
-      let { from } = location.state || { from: { pathname: "/" } };
+      localStorage.setItem('token', data.access_token);
+      const { from } = location.state || { from: { pathname: '/' } };
       history.replace(from);
     });
 };
 
 export const Logout = () => {
-  localStorage.removeItem("token");
+  localStorage.removeItem('token');
 };
 
 export const profile = () => {
-  return customAxios().get("/profile");
+  return customAxios().get('/profile');
 };
