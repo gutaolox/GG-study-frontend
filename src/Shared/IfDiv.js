4c7a893bedfe629d443a2d17606440af5c4c6bb5
gutaolox <32674347+gutaolox@users.noreply.github.com>
2021-04-21 18:00:17 -0300

    if div

---
+++ src/Shared/IfDiv.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const IfDiv = ({ condition, children, className }) => {
+  if (condition) {
+    return <div className={className}> {children}</div>;
+  } else {
+    return null;
+  }
+};
---
+++ src/Utils/constants.js
@@ -0,0 +1,4 @@
+export const USER_ROLES = {
+  PROFESSOR: 1,
+  STUDENT: 2,
+};
